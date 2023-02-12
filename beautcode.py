9b2984fada2c8f73692c37dfbb268218b9191dde
gom6s <gmensah025@gmail.com>
2023-02-12 05:09:43 +0000

    Pycodestyle compliant

---
+++ beautcode.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This module contains sample code that passes the pycodestyle checks"""
+
+
+def print_name(name):
+    """
+    Takes in name, and prints to the stdout.
+    """
+    print(name)
