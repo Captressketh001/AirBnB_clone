fdc55796e998f8e3a3200b4373ef5d50a8df967d
Captressketh001 <shininglite2012@gmail.com>
2023-02-13 15:33:02 +0100

    wrote show command

--- console.py
+++ console.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
 
 import cmd
-from models import base_model
 from models.base_model import BaseModel
 from models import storage
+from models.user import User
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.state import State
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
     """
      a program called console.py that contains the entry point of the command interpreter
     """
     prompt = '(hbnb) '
+    __classes = {
+        "BaseModel",
+        "User",
+        "Place",
+        "City",
+        "Amenity",
+        "State",
+        "Review"
+    }
 
     def do_quit(self, line):
         """
@@ -21,6 +35,7 @@ class HBNBCommand(cmd.Cmd):
         """
         Exiting the program with EOF (ctrl+D)
         """
+        print("")
         return True
 
     def emptyline(self):
@@ -62,6 +77,34 @@ class HBNBCommand(cmd.Cmd):
         except NameError:
             print("** class doesn't exist **")
     
+    def do_show(self, line):
+        """
+        Prints the string representation of an instance based on the class name and id
+        """
+        if not line:
+            print("** class name missing **")
+            return
+        
+        my_line = line.split(" ")
+
+        if my_line[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+            return
+        
+        if len(my_line) == 1:
+            print("** instance id missing **")
+            return
+        
+        key = "{}.{}".format(my_line[0], my_line[1])
+        store = storage.all()
+
+        if key not in store:
+            print("** no instance found **")
+            return
+
+        obj = store[key]
+        print(obj)
+
 
 
 if __name__ == '__main__':
