6a257432ce8235a78d931127a1f9b69b2dd01e5e
gom6s <gmensah025@gmail.com>
2023-02-13 02:26:24 +0000

    Fix pycodestyle non-compliance

--- beautcode.py
+++ beautcode.py
@@ -1,12 +1,3 @@
 #!/usr/bin/env python3
-"""This module contains sample code that passes the pycodestyle checks"""
-
-
-def print_name(name):
-    """
-    Takes in name, and prints to the stdout.
-    """
-    print(name)
-
-
+from beautfunc import print_name
 print_name("Fountain")
---
+++ beautfunc.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""This module contains sample code that passes the pycodestyle checks"""
+if __name__ != "__main__":
+    exit
+
+
+def print_name(name):
+    """
+    Takes in name, and prints to the stdout.
+    Args:
+        name: name of anything
+    """
+    print(name)
