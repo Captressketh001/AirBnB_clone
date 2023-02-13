b35cb956a61c99ddcc164fb6f248e12008778027
gom6s <gmensah025@gmail.com>
2023-02-13 03:56:43 +0000

    Fixed a bug

--- models/base_model.py
+++ models/base_model.py
@@ -41,7 +41,7 @@ class BaseModel():
             """
             New instance
             """
-            self.id = str(uuid.uuid(4))
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
 
