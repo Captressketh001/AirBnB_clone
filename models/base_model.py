49007816f29f6044a7c0aaed249e2f4e7900c2f0
Captressketh001 <shininglite2012@gmail.com>
2023-02-13 17:10:44 +0100

    reverted base-model

--- models/base_model.py
+++ models/base_model.py
@@ -1,75 +1,72 @@
 #!/usr/bin/python3
+import uuid
 from datetime import datetime
-from uuid import uuid4
 import models
-
 """
-Parent class to all classes in the AirBnB clone project
+Parent class to all other classes in this project
 """
-
 
 class BaseModel():
-    """Parent class for AirBnB clone project
-    Methods:
+    """
+    defines all common attributes/methods for other classes
+    Public Instance Attributes:
+        id:  string - assign with an uuid when an instance is created
+        created_at: datetime - assign with the current datetime when an instance is created
+        updated_at: datetime - assign with the current datetime when an instance is created 
+        and it will be updated every time you change your object
+    Public Instance Methods:
         __init__(self, *args, **kwargs)
         __str__(self)
-        __save(self)
         __repr__(self)
+        __save__(self)
         to_dict(self)
     """
 
     def __init__(self, *args, **kwargs):
         """
-        Initialize attributes: uuid4, dates when class was created/updated
+        Public instance attribute (id, created_at, updated_at) 
+        assigned when an instance is created
         """
         date_format = '%Y-%m-%dT%H:%M:%S.%f'
         if kwargs:
             for key, value in kwargs.items():
                 if "created_at" == key:
-                    self.created_at = datetime.strptime(kwargs["created_at"],
-                                                        date_format)
-                elif "updated_at" == key:
-                    self.updated_at = datetime.strptime(kwargs["updated_at"],
-                                                        date_format)
+                    self.created_at = datetime.strptime(kwargs["created_at"], date_format)
+                elif "updated_at":
+                    self.updated_at = datetime.strptime(kwargs["updated_at"], date_format)
                 elif "__class__" == key:
                     pass
                 else:
                     setattr(self, key, value)
         else:
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
         """
-        Return class name, id, and the dictionary
+        Prints the class name, id and dict
         """
-        return ('[{}] ({}) {}'.
-                format(self.__class__.__name__, self.id, self.__dict__))
-
+        return ('[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__))
+    
     def __repr__(self):
         """
-        returns string repr
+        return string representation
         """
         return (self.__str__())
 
     def save(self):
-        """
-        Instance method to:
-        - update current datetime
-        - invoke save() function &
-        - save to serialized file
+        """"
+        updates the public instance attribute updated_at with the current datetime
         """
         self.updated_at = datetime.now()
-        models.storage.save()
 
     def to_dict(self):
         """
-        Return dictionary of BaseModel with string formats of times
+        returns a dictionary containing all keys/values of __dict__ of the instance
         """
-        dic = self.__dict__.copy()
-        dic["created_at"] = self.created_at.isoformat()
-        dic["updated_at"] = self.updated_at.isoformat()
-        dic["__class__"] = self.__class__.__name__
-        return dic
+        result = self.__dict__.copy()
+        result['__class__'] = self.__class__.__name__
+        result['created_at'] = self.created_at.isoformat()
+        result['updated_at'] = self.updated_at.isoformat()
+        return result
