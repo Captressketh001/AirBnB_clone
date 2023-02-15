6884a6de58898a95b8f69976fab45d3a5760be52
gom6s <gmensah025@gmail.com>
2023-02-15 02:50:26 +0000

    fixed bugs

--- models/base_model.py
+++ models/base_model.py
@@ -1,74 +1,49 @@
 #!/usr/bin/python3
-import uuid
-from datetime import datetime
+"""Defines the BaseModel class."""
 import models
-"""
-Parent class to all other classes in this project
-"""
+from uuid import uuid4
+from datetime import datetime
 
-class BaseModel():
-    """
-    defines all common attributes/methods for other classes
-    Public Instance Attributes:
-        id:  string - assign with an uuid when an instance is created
-        created_at: datetime - assign with the current datetime when an instance is created
-        updated_at: datetime - assign with the current datetime when an instance is created 
-        and it will be updated every time you change your object
-    Public Instance Methods:
-        __init__(self, *args, **kwargs)
-        __str__(self)
-        __repr__(self)
-        __save__(self)
-        to_dict(self)
-    """
+
+class BaseModel:
+    """Represents the BaseModel of the HBnB project."""
 
     def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel.
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
         """
-        Public instance attribute (id, created_at, updated_at) 
-        assigned when an instance is created
-        """
-        date_format = '%Y-%m-%dT%H:%M:%S.%f'
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        if kwargs:
-            for key, value in kwargs.items():
-                if "created_at" == key:
-                    self.created_at = datetime.strptime(kwargs["created_at"], date_format)
-                elif "updated_at":
-                    self.updated_at = datetime.strptime(kwargs["updated_at"], date_format)
-                elif "__class__" == key:
-                    pass
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
                 else:
-                    setattr(self, key, value)
+                    self.__dict__[k] = v
         else:
             models.storage.new(self)
 
-    def __str__(self):
-        """
-        Prints the class name, id and dict
-        """
-        return ('[{}] ({}) {}'.
-                format(self.__class__.__name__, self.id, self.__dict__))
-    
-    def __repr__(self):
-        """
-        return string representation
-        """
-        return (self.__str__())
-
     def save(self):
-        """"
-        updates the public instance attribute updated_at with the current datetime
-        """
-        self.updated_at = datetime.now()
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
+        """Return the dictionary of the BaseModel instance.
+        Includes the key/value pair __class__ representing
+        the class name of the object.
         """
-        returns a dictionary containing all keys/values of __dict__ of the instance
-        """
-        result = self.__dict__.copy()
-        result['__class__'] = self.__class__.__name__
-        result['created_at'] = self.created_at.isoformat()
-        result['updated_at'] = self.updated_at.isoformat()
-        return result
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
